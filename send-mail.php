15c134c47a0b3b9c01bde711d902d1a658381221
Anushka Maharjan <anushkamaharjan96.am@gmail.com>
2023-03-28 13:59:16 +0545

    send mail

---
+++ send-mail.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+//PHPMailer library is required to send email from localhost
+$mail = new PHPMailer(true);
+
+    $mail->SMTPDebug = 2;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'amaharjan.logispark@gmail.com';
+    //get password from gmail, follow https://www.getmailbird.com/gmail-app-password/ to generate your app password
+    //2 factor authentication should be activated to generate app password
+    $mail->Password   = 'ldsktdyksfnintws';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587;
+
+    $mail->setFrom('amaharjan.logispark@gmail.com', 'Anushka');
+    $mail->addAddress('amaharjan.logispark@gmail.com');
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Subject';
+    $mail->Body    = 'HTML message body in <b>bold</b> ';
+    $mail->AltBody = 'Body in plain text for non-HTML mail clients';
+    if($mail->send()){
+        echo 'mail sent';
+    }
+
+
+?>
